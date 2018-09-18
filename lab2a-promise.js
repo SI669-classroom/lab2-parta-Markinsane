45a4e5752e3ed86465549217cc7e369da8a86560
Markinsane <markinsane@github.com>
2018-09-17 22:08:30 -0400

    Lab#2

--- lab2a-promise.js
+++ lab2a-promise.js
@@ -10,21 +10,38 @@ function flip() {
 
 //// Add code here that will "flip" the coin ten times and write the 
 //// result to the console (e.g. "Heads" or "Tails" for each flip).
+for (let i = 0; i < 10; i++) {
+    
+    // let p = new Promise((resolve, reject)=>{ 
+   
+    // if (flip()) resolve();
+    // else reject()
+    // });           // what happens if you remove this?
 
+    flip().then(()=> {console.log("Head"); console.log("Tails")})
+    
+
+    // .catch((err)=> {console.log("tails")})
+}
 
 
 
 ///// Problem 2 - More fun...
 
 function countBig(bignum) {
+	return new Promise( (resolve,reject) => { 
+		setTimeout(() => {resolve()}, bignum)
+
+	} )  
     // Add code here that returns a Promise that will resolve after it has counted to bignum
 }
 
 
 start = Date.now();
 bignum = 1000000000;
-countBig(1000000000).then(()=> {
+countBig(1000).then(()=> {
     console.log("It took " + (Date.now() - start) + " ms to count to " + bignum);
 }, () => {
     console.log("A problem occurred while trying to count to " + bignum);
 })
+
